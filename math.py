66a44abeae852e458f4053ffa70d20d089a80592
ganesh manish gopale <57770556+ganesh208@users.noreply.github.com>
2022-03-03 10:23:03 +0530

    intial commit of original file

---
+++ math.py
@@ -0,0 +1,25 @@
+
+#Add Implementation
+def add(x,y):
+    return x+y
+    return x+y
+    
+#Substract Implementation
+def substract(x,y):
+    return x-y
+    
+#Multiply Implementation
+def multiply(x,y):
+    return x*y
+
+#Divide Implementation
+def divide(x,y):
+    if y==0
+    return divide by zero error
+    else
+    return x/y
+    
+#square implementation    
+def square(x):   
+    return x*x
+    
